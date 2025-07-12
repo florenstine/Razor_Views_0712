ede087faaa5d6aaa775761270bb13470392266bb
EricJamesSonio <ericjamessonio7@gmail.com>
2025-07-12 09:44:47 +0800

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace View_Appsdev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
